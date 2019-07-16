3aeddcb9f994d8968ca13f44f8c652222e058859
dongxinjie <dongxinjie@fcbox.com>
2019-07-16 15:36:26 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # frontend-maven-plugin-demo
 frontend-maven-plugin（npm和yarn两种）
+
+[插件官网](http://www.worldlink.com.cn/osdir/frontend-maven-plugin.html)
---
+++ boot-server/src/main/java/com/example/bootserver/BootServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootServerApplication.class, args);
+	}
+
+}
